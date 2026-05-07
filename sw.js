55eadf6a78549e986295014d750400c9c90875f6
Lucas_Dantas <lucascarvdan@gmail.com>
2026-05-07 18:09:51 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,11 @@
-const CACHE_NAME = "syscadeiras-cache-v2";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/index_app.html",
-  "/style.css",
-  "/script.js",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  self.skipWaiting();
+// Remove este Service Worker e todos os caches instalados anteriormente
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll())
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
   );
 });
